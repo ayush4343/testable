37195dbdf9bfc10b34c70a553e96303b68c4637c
Ayush Jain <ayush.jain@taskcraft.in>
2023-12-05 16:28:10 +0530

    baseline

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,60 +1,60 @@
+# frozen_string_literal: true
+
 class PostsController < ApplicationController
-    # frozen_string_literal: true
-    before_action :set_post, only: %i[show update destroy]
-  
-    # GET /vehicles
-    def index
-      @posts = Post.all
-  
-      render json: @posts
+  # frozen_string_literal: true
+  before_action :set_post, only: %i[show update destroy]
+
+  # GET /vehicles
+  def index
+    @posts = Post.all
+
+    render json: @posts
+  end
+
+  # GET /vehicles/1
+  def show
+    render json: @post
+  end
+
+  # POST /vehicles
+  def create
+    @post = Post.new(vehicle_params)
+
+    if @post.save
+      render json: @post, status: :created
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
-  
-    # GET /vehicles/1
-    def show
+  end
+
+  # PATCH/PUT /vehicles/1
+  def update
+    if @post.update(post_params)
+      @post.update(group_id: nil) if @vehicle.group_id.zero?
       render json: @post
+    else
+      render json: @post.errors, status: :unprocessable_entity
     end
-  
-    # POST /vehicles
-    def create
-      @post = Post.new(vehicle_params)
-  
-      if @post.save
-        render json: @post, status: :created
-      else
-        render json: @post.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # PATCH/PUT /vehicles/1
-    def update
-      if @post.update(post_params)
-        @post.update(group_id: nil) if @vehicle.group_id == 0
-        render json: @post
-      else
-        render json: @post.errors, status: :unprocessable_entity
-      end
-    end
-  
-    # DELETE /vehicles/1
-    def destroy
-      @post.destroy
-    end
-  
-    private
-  
-    # Use callbacks to share common setup or constraints between actions.
-    def set_post
-      @post = Post.find(params[:id])
-    end
-  
-    # Only allow a trusted parameter "white list" through.
-    def post_params
-      params.require(:post).permit(
-        :name,
-        :email,
-        :address,
-      )
-    end
-  
-  
+  end
+
+  # DELETE /vehicles/1
+  def destroy
+    @post.destroy
+  end
+
+  private
+
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Only allow a trusted parameter "white list" through.
+  def post_params
+    params.require(:post).permit(
+      :name,
+      :email,
+      :address
+    )
+  end
 end
